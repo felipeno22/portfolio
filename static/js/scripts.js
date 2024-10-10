7d1aaec4e154e60a8aa95e5357178fcdf3af4c13
sarinhax <santinha_sarinhax23@outlook.com>
2024-10-10 10:15:02 -0400

    ajustando js e css do meu porfolio e inserindo form contato

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,6 +1,30 @@
 const mobileMenu = document.getElementById('mobile-menu');
 const menu = document.querySelector('.menu');
 
+// Alternar o menu com um clique (usado no celular e no desktop)
 mobileMenu.addEventListener('click', () => {
     menu.classList.toggle('active');
+});
+
+// Mostrar o menu ao passar o mouse
+mobileMenu.addEventListener('mouseover', () => {
+    menu.classList.add('active');
+});
+
+menu.addEventListener('touchstart', () => {
+    menu.classList.add('active');
+});
+
+// Fechar o menu ao clicar fora dele (para celular e desktop)
+document.addEventListener('click', (e) => {
+    if (!menu.contains(e.target) && !mobileMenu.contains(e.target)) {
+        menu.classList.remove('active');
+    }
+});
+
+// Fechar o menu ao selecionar uma opção
+document.querySelectorAll('.menu li a').forEach(item => {
+    item.addEventListener('click', () => {
+        menu.classList.remove('active');
+    });
 });
